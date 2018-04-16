8b4012719493b3e7a009c4945dfb9edfe137c748
Vali Ghita <valx92@gmail.com>
2018-04-16 23:05:00 +0300

    labs: lab08: add filesystem part1 lab template

--- tools/labs/templates/filesystems/minfs/kernel/minfs.h
+++ tools/labs/templates/filesystems/minfs/kernel/minfs.h
@@ -1,16 +1,12 @@
 #ifndef _MINFS_H
 #define _MINFS_H	1
 
-#define MINFS_MAGIC		0xDEADF00D
+#define MINFS_MAGIC 		0xDEADF00D
 #define MINFS_NAME_LEN		16
 #define MINFS_BLOCK_SIZE	4096
-#define MINFS_NUM_INODES	32
-#define MINFS_NUM_ENTRIES	32
-
-#define MINFS_ROOT_INODE	0
 
 /*
- * Filesystem layout:
+ * filesystem layout:
  *
  *      SB      IZONE 	     DATA
  *    ^	    ^ (1 block)
@@ -18,14 +14,9 @@
  *    +-0   +-- 4096
  */
 
-#define MINFS_SUPER_BLOCK	0
-#define MINFS_INODE_BLOCK	1
-#define MINFS_FIRST_DATA_BLOCK	2
-
 struct minfs_super_block {
 	unsigned long magic;
 	__u8 version;
-	unsigned long imap;
 };
 
 struct minfs_dir_entry {
@@ -33,7 +24,7 @@ struct minfs_dir_entry {
 	char name[MINFS_NAME_LEN];
 };
 
-/* A minfs inode uses a single block. */
+/* an minfs inode uses a single block */
 struct minfs_inode {
 	__u32 mode;
 	__u32 uid;
@@ -42,4 +33,4 @@ struct minfs_inode {
 	__u16 data_block;
 };
 
-#endif /* _MINFS_H */
+#endif
