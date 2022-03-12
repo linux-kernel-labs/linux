cd67430fa46560ca346da83140d77d4bf728740e
Georgian123123 <georgiancomana1119@gmail.com>
2022-03-13 00:10:25 +0200

    Laborator2: ex3: Solved 3rd exercice
    Signed-off-by: Georgian123123 <georgiancomana1119@gmail.com>

--- tools/labs/skels/kernel_api/3-memory/memory.c
+++ tools/labs/skels/kernel_api/3-memory/memory.c
@@ -25,20 +25,26 @@ static struct task_info *task_info_alloc(int pid)
 	struct task_info *ti;
 
 	/* TODO 1: allocated and initialize a task_info struct */
+	ti = kmalloc(sizeof(*ti), GFP_KERNEL);
+	ti->pid = pid;
+	ti->timestamp = jiffies;
 
 	return ti;
 }
 
 static int memory_init(void)
 {
+	struct task_struct *task = current;
 	/* TODO 2: call task_info_alloc for current pid */
-
+	ti1 = task_info_alloc(task->pid);
 	/* TODO 2: call task_info_alloc for parent PID */
-
+	ti2 = task_info_alloc(task->real_parent->pid);
 	/* TODO 2: call task_info alloc for next process PID */
-
+	task = next_task(task);
+	ti3 = task_info_alloc(task->pid);
 	/* TODO 2: call task_info_alloc for next process of the next process */
-
+	task = next_task(task);
+	ti4 = task_info_alloc(task->pid);
 	return 0;
 }
 
@@ -46,8 +52,16 @@ static void memory_exit(void)
 {
 
 	/* TODO 3: print ti* field values */
+	printk("Ti1: PID = %d,  timestamp = %ld\n", ti1->pid, ti1->timestamp);
+	printk("Ti2: PID = %d,  timestamp = %ld\n", ti2->pid, ti2->timestamp);
+	printk("Ti3: PID = %d,  timestamp = %ld\n", ti3->pid, ti3->timestamp);
+	printk("Ti4: PID = %d,  timestamp = %ld\n", ti4->pid, ti4->timestamp);
 
 	/* TODO 4: free ti* structures */
+	kfree(ti1);
+	kfree(ti2);
+	kfree(ti3);
+	kfree(ti4);
 }
 
 module_init(memory_init);
