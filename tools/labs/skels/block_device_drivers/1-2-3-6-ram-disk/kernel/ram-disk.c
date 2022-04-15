8186423b301f30c3d522d9a20d6ec4cf27cbff0f
Cristian Olaru <cristianolaru99@gmail.com>
2022-04-15 09:16:29 +0300

    lab 7: ex 2
    Signed-off-by: Cristian Olaru <cristianolaru99@gmail.com>

--- tools/labs/skels/block_device_drivers/1-2-3-6-ram-disk/kernel/ram-disk.c
+++ tools/labs/skels/block_device_drivers/1-2-3-6-ram-disk/kernel/ram-disk.c
@@ -86,12 +86,25 @@ static blk_status_t my_block_request(struct blk_mq_hw_ctx *hctx,
 	struct my_block_dev *dev = hctx->queue->queuedata;
 
 	/* TODO 2: get pointer to request */
+	rq = bd->rq;
 
 	/* TODO 2: start request processing. */
+	blk_mq_start_request(rq);
 
 	/* TODO 2: check fs request. Return if passthrough. */
+	if (blk_rq_is_passthrough(rq)) {
+		printk(KERN_NOTICE "Skip non-fs request\n");
+		blk_mq_end_request(rq, BLK_STS_IOERR);
+		goto out;
+	}
 
 	/* TODO 2: print request information */
+	printk(KERN_LOG_LEVEL
+		"request info: pos=%llu bytes=%u "
+		"cur_bytes=%u dir=%s\n",
+		(unsigned long long) blk_rq_pos(rq),
+		blk_rq_bytes(rq), blk_rq_cur_bytes(rq),
+		rq_data_dir(rq) ? "Write" : "Read");
 
 #if USE_BIO_TRANSFER == 1
 	/* TODO 6: process the request by calling my_xfer_request */
@@ -100,6 +113,7 @@ static blk_status_t my_block_request(struct blk_mq_hw_ctx *hctx,
 #endif
 
 	/* TODO 2: end request successfully */
+	blk_mq_end_request(rq, BLK_STS_OK);
 
 out:
 	return BLK_STS_OK;
@@ -186,11 +200,15 @@ static int __init my_block_init(void)
 	}
 
 	/* TODO 2: create block device using create_block_device */
+	err = create_block_device(&g_dev);
+	if (err < 0)
+		goto out;
 
 	return 0;
 
 out:
 	/* TODO 2: unregister block device in case of an error */
+	unregister_blkdev(MY_BLOCK_MAJOR, MY_BLKDEV_NAME);
 	return err;
 }
 
@@ -212,6 +230,7 @@ static void delete_block_device(struct my_block_dev *dev)
 static void __exit my_block_exit(void)
 {
 	/* TODO 2: cleanup block device using delete_block_device */
+	delete_block_device(&g_dev);
 
 	/* TODO 1: unregister block device */
 	unregister_blkdev(MY_BLOCK_MAJOR, MY_BLKDEV_NAME);
